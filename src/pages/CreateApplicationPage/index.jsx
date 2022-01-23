a81adccdafd1fd5b79efef129e00774ee8ad5f35
elyase <elyase.saglam@ogr.sakarya.edu.tr>
2022-01-23 12:18:07 +0300

    basvuru-olustur sayfası olusturuldu

--- src/pages/CreateApplicationPage/index.jsx
+++ src/pages/CreateApplicationPage/index.jsx
@@ -1,7 +1,88 @@
 import React from 'react';
+import { useFormik, FormikProvider } from 'formik';
+import * as Yup from 'yup';
+import FormInput from '../../components/Input';
+import './index.css';
 
 function CreateApplicationPage() {
-  return <div>CreateApplicationPage</div>;
+  const tcRegExp = /^[1-9]{1}[0-9]{9}[02468]{1}$/;
+
+  const SignupSchema = Yup.object().shape({
+    isim: Yup.string()
+      .min(2, 'İsminizi Doğru Giriniz')
+      .max(50, 'İsminiz 50 karakterden fazla olamaz')
+      .required('Bu alanı doldurmanız zorunludur'),
+    soyisim: Yup.string()
+      .min(2, 'Soyisminizi Doğru Giriniz')
+      .max(50, 'Soyisminiz 50 karakterden fazla olamaz')
+      .required('Bu alanı doldurmanız zorunludur'),
+    yas: Yup.number()
+      .max(120, 'Lütfen Gerçek Yaşınızı Giriniz')
+      .positive('Yaşınız pozitif değere sahip olmalıdır')
+      .required('Bu alanı doldurmanız zorunludur'),
+    tc: Yup.string()
+      .matches(tcRegExp, 'Kimlik Numarası Geçersiz')
+      .required('Bu alanı doldurmanız zorunludur'),
+    adres: Yup.string()
+      .min(12, 'Lütfen Adresinizi Doğru Giriniz')
+      .required('Bu alanı doldurmanız zorunludur'),
+    basvuru: Yup.string()
+      .min(10, 'Lütfen Basvurunuzu Doğru Açıklayınız')
+      .required('Bu alanı doldurmanız zorunludur'),
+  });
+
+  const formik = useFormik({
+    initialValues: {
+      isim: '',
+      soyisim: '',
+      yas: '',
+      tc: '',
+      adres: '',
+      basvuru: '',
+    },
+    validationSchema: SignupSchema,
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
+  return (
+    <FormikProvider value={formik}>
+      <form onSubmit={formik.handleSubmit}>
+        <h2>Başvuru Oluştur</h2>
+        <FormInput type="text" placeholder="İsminiz" title="İsim" name="isim" />
+        <FormInput
+          type="text"
+          placeholder="Soyisminiz"
+          title="Soyisim"
+          name="soyisim"
+        />
+        <FormInput type="number" placeholder="Yaşınız" title="Yaş" name="yas" />
+        <FormInput
+          type="string"
+          placeholder="Kimlik Numaranız"
+          title="TC Kimlik Numarası"
+          name="tc"
+        />
+        <FormInput
+          type="textarea"
+          placeholder="Adresiniz"
+          title="Adresiniz"
+          name="adres"
+        />
+        <FormInput
+          type="textarea"
+          placeholder="Başvuru Nedeniniz"
+          title="Başvuru Nedeniniz"
+          id="textarea"
+          name="basvuru"
+        />
+        <button type="submit" className="submit">
+          Submit
+        </button>
+      </form>
+    </FormikProvider>
+  );
 }
 
 export default CreateApplicationPage;
