ef55f519baa5eb436338c97672c73d31c690aa57
elyase <elyase.saglam@ogr.sakarya.edu.tr>
2022-01-28 15:53:10 +0300

    başvuru oluşturma sayfası tamamlandı

--- src/pages/CreateApplicationPage/index.jsx
+++ src/pages/CreateApplicationPage/index.jsx
@@ -8,6 +8,7 @@ import { create } from '../../redux/createApplicationSlice';
 import db from '../../firebase';
 import createApplicationSchema from '../../validations/Application';
 import FormInput from '../../components/Input';
+import CustomButton from '../../components/Button';
 
 function CreateApplicationPage() {
   const dispatch = useDispatch();
@@ -17,10 +18,15 @@ function CreateApplicationPage() {
     const docRef = await addDoc(collection(db, 'applications'), payload);
     const applicationData = await getDoc(doc(db, 'applications', docRef.id));
     if (applicationData.exists()) {
-      dispatch(create(applicationData.data()));
+      dispatch(
+        create({
+          ...applicationData.data(),
+          id: applicationData.id,
+        })
+      );
       navigate('/basvuru-basarili');
     } else {
-      console.log('Veri çekilirken hata oluştu.');
+      alert('Veri çekilirken hata oluştu.');
     }
   };
 
@@ -35,44 +41,59 @@ function CreateApplicationPage() {
     },
     validationSchema: createApplicationSchema,
     onSubmit: (values) => {
-      handleUpload(values);
+      handleUpload({
+        ...values,
+        createdAt: new Date().getTime(),
+        status: 'bekliyor',
+        answers: [],
+      });
     },
   });
 
   return (
     <FormikProvider value={formik}>
-      <form onSubmit={formik.handleSubmit}>
-        <h2>Başvuru Oluştur</h2>
-        <FormInput type="text" placeholder="İsminiz" title="İsim" name="isim" />
-        <FormInput
-          type="text"
-          placeholder="Soyisminiz"
-          title="Soyisim"
-          name="soyisim"
-        />
-        <FormInput type="number" placeholder="Yaşınız" title="Yaş" name="yas" />
-        <FormInput
-          type="string"
-          placeholder="Kimlik Numaranız"
-          title="TC Kimlik Numarası"
-          name="tc"
-        />
-        <FormInput
-          type="textarea"
-          placeholder="Adresiniz"
-          title="Adresiniz"
-          name="adres"
-        />
-        <FormInput
-          type="textarea"
-          placeholder="Başvuru Nedeniniz"
-          title="Başvuru Nedeniniz"
-          name="basvuru"
-        />
-        <button type="submit" className="submit">
-          Submit
-        </button>
-      </form>
+      <div className="create-wrapper">
+        <form onSubmit={formik.handleSubmit}>
+          <h2>Başvuru Oluştur</h2>
+          <FormInput
+            type="text"
+            placeholder="İsminiz"
+            title="İsim"
+            name="isim"
+          />
+          <FormInput
+            type="text"
+            placeholder="Soyisminiz"
+            title="Soyisim"
+            name="soyisim"
+          />
+          <FormInput
+            type="number"
+            placeholder="Yaşınız"
+            title="Yaş"
+            name="yas"
+          />
+          <FormInput
+            type="string"
+            placeholder="Kimlik Numaranız"
+            title="TC Kimlik Numarası"
+            name="tc"
+          />
+          <FormInput
+            type="textarea"
+            placeholder="Adresiniz"
+            title="Adresiniz"
+            name="adres"
+          />
+          <FormInput
+            type="textarea"
+            placeholder="Başvuru Nedeniniz"
+            title="Başvuru Nedeniniz"
+            name="basvuru"
+          />
+          <CustomButton type="submit">Gönder</CustomButton>
+        </form>
+      </div>
     </FormikProvider>
   );
 }
